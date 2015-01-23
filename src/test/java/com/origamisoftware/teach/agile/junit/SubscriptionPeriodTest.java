43433a4a77670fa71ab66bc6e38481565eb4cc53
spencermarks <spencer.marks@claritasgenomics.com>
2015-01-22 21:25:24 -0500

    another attempt at fixing the code

--- src/test/java/com/origamisoftware/teach/agile/junit/SubscriptionPeriodTest.java
+++ src/test/java/com/origamisoftware/teach/agile/junit/SubscriptionPeriodTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import java.util.Calendar;
 
 import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
  * @author Spencer A Marks
@@ -72,7 +72,7 @@ public class SubscriptionPeriodTest {
      */
     @Test
     public void testHashExpired() {
-        assertTrue(true);
+       assertFalse(false);
     }
 
 
